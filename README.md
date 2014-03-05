c4f31ccbfcdedf37fdf0782d376925ab831dd729
Will Webberley <willwebberley@gmail.com>
2014-03-05 14:13:53 +0000

    Added README and comments to code

---
+++ README.md
@@ -0,0 +1,31 @@
+NodeDirectUploader
+===================
+
+Licensing
+----------
+
+*General*: The files in this repository are, unless stated otherwise, released under the Apache License. You are free to redistribute this code with or without modification. The full license text is available [here](http://www.apache.org/licenses/LICENSE-2.0). 
+
+*Other*: The script `s3upload.js` is copyright 2013 tadruj. Please find more information on this script at its home [repository](https://github.com/tadruj/s3upload-coffee-javascript). It is released under the Apache License, and the full license text is available [here](http://www.apache.org/licenses/LICENSE-2.0).
+
+
+Direct-to-S3 uploads in a Node.js application running on Heroku
+-----------------------------------------------------------------------------
+
+Simple example demonstrating how to accomplish a direct upload to Amazon's S3 in a Node.js web application.
+
+This example uses the [express](http://expressjs.com/) web framework to facilitate request-handling. However, the process of signing the S3 PUT request would be identical in most Node apps.
+
+This code is mostly ready to be run as cloned, but a function `update_account()` will need to be defined to handle the storing of the POSTed information. The current example simply demonstrates the upload to S3.
+
+
+**Running the application**
+* Set environment variables for your AWS access key and secret (see Heroku's  [Config Vars article](https://devcenter.heroku.com/articles/config-vars))
+* Download and install the [Heroku toolbelt](https://toolbelt.heroku.com/)
+* Run with ```$ foreman start```
+
+**Deploying the application (see the article [Deploying with Git](https://devcenter.heroku.com/articles/git) for more detailed information)**
+* Download and install the [Heroku toolbelt](https://toolbelt.heroku.com/)
+* Commit your application to a local Git repository (e.g. `git init`, `git add .`, `git commit -m "version 1 commit"`, etc.)
+* Create the application on Heroku by adding a Git remote (`$ heroku create`)
+* Push your code to the new Heroku repo (`$ git push heroku master`)
--- app.js
+++ app.js
@@ -1,23 +1,51 @@
+/*
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+
+/*
+ * Import required packages.
+ * Packages should be installed with "npm install".
+ */
 var express = require('express');
 var crypto = require('crypto');
 var http = require('http');
 var path = require('path');
 
+/*
+ * Set-up the Express app.
+ */
 var app = express();
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'public')));
 
+/*
+ * Load the S3 information from the environment variables.
+ */
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET
+
+/*
+ * Respond to GET requests to /account.
+ * Upon request, render the 'account.html' web page in views/ directory.
+ */
 app.get('/account', function(req, res){
     res.render('account.html');
 });
 
+/*
+ * Respond to GET requests to /sign_s3.
+ * Upon request, return JSON containing the temporarily-signed S3 request and the
+ * anticipated URL of the image.
+ */
 app.get('/sign_s3', function(req, res){
-    var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
-    var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
-    var S3_BUCKET = process.env.S3_BUCKET;
-
     var object_name = req.query.s3_object_name;
     var mime_type = req.query.s3_object_type;
 
@@ -40,12 +68,20 @@ app.get('/sign_s3', function(req, res){
     res.end();
 });
 
+/*
+ * Respond to POST requests to /submit_form.
+ * This function needs to be completed to handle the information in 
+ * a way that suits your application.
+ */
 app.post('/submit_form', function(req, res){
     username = req.body.username;
     full_name = req.body.full_name;
     avatar_url = req.body.avatar_url;
-    create_account(username, full_name, avatar_url); // TODO: create this function
+    update_account(username, full_name, avatar_url); // TODO: create this function
     // TODO: Return something useful or redirect
 });
 
+/*
+ * Start the server to handle incoming requests.
+ */
 app.listen(app.get('port'));
