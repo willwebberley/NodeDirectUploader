d507219aa8938cfcfd7a6171a7edff8700b50802
Will Webberley <willwebberley@gmail.com>
2016-05-23 20:11:45 +0100

    moved to es6 syntax

--- app.js
+++ app.js
@@ -11,62 +11,58 @@
  * Import required packages.
  * Packages should be installed with "npm install".
  */
-var express = require('express');
-var path = require('path');
-var aws = require('aws-sdk');
+const express = require('express');
+const aws = require('aws-sdk');
 
 /*
- * Set-up the Express app.
+ * Set-up and run the Express app.
  */
-var app = express();
-app.set('views', __dirname + '/views');
+const app = express();
+app.set('views', './views');
+app.use(express.static('./public'));
 app.engine('html', require('ejs').renderFile);
-app.set('port', process.env.PORT || 3000);
-app.use(express.static(path.join(__dirname, 'public')));
+app.listen(process.env.PORT || 3000);
 
 /*
  * Load the S3 information from the environment variables.
  */
-var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
-var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
-var S3_BUCKET = process.env.S3_BUCKET;
+const S3_BUCKET = process.env.S3_BUCKET;
 
 /*
  * Respond to GET requests to /account.
  * Upon request, render the 'account.html' web page in views/ directory.
  */
-app.get('/account', function(req, res){
-    res.render('account.html');
-});
+app.get('/account', (req, res) => res.render('account.html'));
 
 /*
- * Respond to GET requests to /sign_s3.
- * Upon request, return JSON containing the temporarily-signed S3 request and the
- * anticipated URL of the image.
+ * Respond to GET requests to /sign-s3.
+ * Upon request, return JSON containing the temporarily-signed S3 request and
+ * the anticipated URL of the image.
  */
-app.get('/sign_s3', function(req, res){
-    aws.config.update({accessKeyId: AWS_ACCESS_KEY , secretAccessKey: AWS_SECRET_KEY });
-    var s3 = new aws.S3();
-    var s3_params = {
-        Bucket: S3_BUCKET,
-        Key: req.query.file_name,
-        Expires: 60,
-        ContentType: req.query.file_type,
-        ACL: 'public-read'
+app.get('/sign-s3', (req, res) => {
+  const s3 = new aws.S3();
+  const fileName = req.query['file-name'];
+  const fileType = req.query['file-type'];
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 60,
+    ContentType: fileType,
+    ACL: 'public-read'
+  };
+
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if(err){
+      console.log(err);
+      return res.end();
+    }
+    const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
     };
-    s3.getSignedUrl('putObject', s3_params, function(err, data){
-        if(err){
-            console.log(err);
-        }
-        else{
-            var return_data = {
-                signed_request: data,
-                url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name
-            };
-            res.write(JSON.stringify(return_data));
-            res.end();
-        }
-    });
+    res.write(JSON.stringify(returnData));
+    res.end();
+  });
 });
 
 /*
@@ -74,15 +70,6 @@ app.get('/sign_s3', function(req, res){
  * This function needs to be completed to handle the information in
  * a way that suits your application.
  */
-app.post('/submit_form', function(req, res){
-    username = req.body.username;
-    full_name = req.body.full_name;
-    avatar_url = req.body.avatar_url;
-    update_account(username, full_name, avatar_url); // TODO: create this function
-    // TODO: Return something useful or redirect
+app.post('/save-details', (req, res) => {
+  // TODO: Read POSTed form data and do something useful
 });
-
-/*
- * Start the server to handle incoming requests.
- */
-app.listen(app.get('port'));
