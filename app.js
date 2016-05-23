64ef682a6feb1046091ef3c08d9c83378643bbb6
Will Webberley <willwebberley@gmail.com>
2016-05-23 19:09:07 +0100

    updated to Express4

--- app.js
+++ app.js
@@ -12,7 +12,6 @@
  * Packages should be installed with "npm install".
  */
 var express = require('express');
-var http = require('http');
 var path = require('path');
 var aws = require('aws-sdk');
 
@@ -47,32 +46,32 @@ app.get('/account', function(req, res){
  */
 app.get('/sign_s3', function(req, res){
     aws.config.update({accessKeyId: AWS_ACCESS_KEY , secretAccessKey: AWS_SECRET_KEY });
-    var s3 = new aws.S3(); 
-    var s3_params = { 
-        Bucket: S3_BUCKET, 
-        Key: req.query.file_name, 
-        Expires: 60, 
-        ContentType: req.query.file_type, 
+    var s3 = new aws.S3();
+    var s3_params = {
+        Bucket: S3_BUCKET,
+        Key: req.query.file_name,
+        Expires: 60,
+        ContentType: req.query.file_type,
         ACL: 'public-read'
-    }; 
-    s3.getSignedUrl('putObject', s3_params, function(err, data){ 
-        if(err){ 
-            console.log(err); 
+    };
+    s3.getSignedUrl('putObject', s3_params, function(err, data){
+        if(err){
+            console.log(err);
         }
-        else{ 
+        else{
             var return_data = {
                 signed_request: data,
-                url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name 
+                url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name
             };
             res.write(JSON.stringify(return_data));
             res.end();
-        } 
+        }
     });
 });
 
 /*
  * Respond to POST requests to /submit_form.
- * This function needs to be completed to handle the information in 
+ * This function needs to be completed to handle the information in
  * a way that suits your application.
  */
 app.post('/submit_form', function(req, res){
